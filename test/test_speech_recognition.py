d292ab2c87a7e85db199b66316c2d9125dc97e1a
Umar Ansari <uans125@gmail.com>
2025-03-14 01:04:23 +0100

    Tested speech to text using whisper

--- test/test_speech_recognition.py
+++ test/test_speech_recognition.py
@@ -1,1 +1,8 @@
-# Test for speech recognition model
+# Test for speech recognition model (speech to text )
+import whisper
+
+# Change models for better reuslts (small model detect correctly and is much faster)
+model = whisper.load_model("small")
+result = model.transcribe("test/recording1.wav")
+print(result["text"])
+
